8d7813bae75a9bb42bfc80493992112b09dc0578
Nahdhiyyah <nahdhiyyah13@gmail.com>
2022-08-08 06:31:56 +0700

    API

---
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request){
+        $validateData = $request->validate([
+            'name' => 'required | max:25',
+            'email' => 'email | required | unique:users',
+            'password' => 'required | confirmed'
+        ]);
+
+        // membuat user
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $user->save();
+        return response()->json($user, 201);
+    }
+
+    public function login(Request $request){
+        $validateData = $request->validate([
+            'email' => 'email | required | unique:users',
+            'password' => 'required | confirmed',
+        ]);
+
+        $login_detail = request(['email', 'password']);
+
+        if (!Auth::attempt([$login_detail])) {
+            return response()->json(
+                [
+                    'error' => 'login gagal. cek kembali data anda'
+                ], 401
+            );
+        }
+
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('AccessToken');
+        $token = $tokenResult->token;
+        $token->save();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_id' => $token->id,
+            'user_id' => $user_id,
+            'name' => $user->name,
+            'email' => $user->email
+        ], 200);
+    }
+}
+
+
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'Api\Auth\AuthController@register')->name('register');
+Route::post('login', 'Api\Auth\AuthController@login')->name('login');
